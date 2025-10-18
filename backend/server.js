88c0588b1e380fbba2f86ecd023bd1c6f20e13ec
Garvit <garvithambiria.17@gmail.com>
2025-10-18 20:11:25 +0530

    PORT error issue solved

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const aiRoutes=require('./routes/aiRoutes');
+const aiRoutes=require('./routes/aiRoutes.js');
 dotenv.config();
 
 
@@ -23,4 +23,4 @@ app.use('/api/projects', require('./routes/projectRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0",() => console.log(`Server running on port ${PORT}`));
